1c38c17a2e8411f2e68b3a7987a1b9215f1a303d
Yixiu Yin <yixiuyin10@gmail.com>
2023-10-14 20:56:18 +0100

    basic route setup

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routes/index')
+const musiciansRouter = require('./routes/musicians')
+const connectRouter = require('./routes/connect')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -12,5 +14,8 @@ app.use(express.static('public'))
 
 app.use('/', indexRouter
 )
+app.use('/musicians', musiciansRouter)
+app.use('/connect', connectRouter)
+
 
 app.listen(process.env.PORT || 3000)
