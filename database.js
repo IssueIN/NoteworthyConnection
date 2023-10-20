8c0d5d4f0217bf392326bd43b4b89d4e73f55d84
Yixiu Yin <yixiuyin10@gmail.com>
2023-10-20 19:07:47 +0100

    aws certs

--- database.js
+++ database.js
@@ -7,6 +7,7 @@ const mysql = require('mysql2')
 
 const pool = mysql.createPool({
   host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME
