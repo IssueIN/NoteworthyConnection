6241ccac85875ddfe7aaafa2d4e3cc9c4cf18a8f
IssueIn <yixiuyin10@gmail.com>
2023-12-04 17:36:34 +0000

    video loop improve

--- public/javascripts/indexVideo.js
+++ public/javascripts/indexVideo.js
@@ -1,9 +1,21 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-  const video = document.getElementById('indexVideo');
+// document.addEventListener('DOMContentLoaded', (event) => {
+//   const video = document.getElementById('indexVideo');
 
-  video.addEventListener('mouseenter', function (e) {
-      if (video.paused) {
-          video.play();
-      }
-  });
+//   video.addEventListener('mouseenter', function (e) {
+//       if (video.paused) {
+//           video.play();
+//       }
+//   });
+// });
+
+document.addEventListener("DOMContentLoaded", function() {
+  var video = document.getElementById("indexVideo");
+
+  // Check if video can play, and play it
+  video.oncanplaythrough = function() {
+      video.muted = true; // Mute the video (required by some browsers for autoplay)
+      video.play();
+  };
+
+  video.loop = true; // Ensure the video loops
 });
