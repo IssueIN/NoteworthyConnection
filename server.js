920c7920e094b4dbd475a4abb811b7c18e663ec9
IssueIn <yixiuyin10@gmail.com>
2024-01-09 18:37:34 +0000

    add bilingual system

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const bodyParser = require('body-parser')
+const i18next = require('i18next')
+const Backend = require('i18next-fs-backend')
+const middleware = require('i18next-http-middleware')
+
+i18next.use(Backend).use(middleware.LanguageDetector)
+  .init({
+    fallbacklang: 'en',
+    backend: {
+      loadPath: __dirname + '/locales/{{lng}}/{{ns}}.json',
+    },
+    preload: ['en', 'zh']
+})
 
 const indexRouter = require('./routes/index')
 const musiciansRouter = require('./routes/musicians')
@@ -14,6 +26,7 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(middleware.handle(i18next));
 
 app.use('/', indexRouter)
 app.use('/musicians', musiciansRouter)
